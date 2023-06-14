c6786a4bab52173cfd5647563daa08612a0733c0
Takatomo Fujisawa <fujisawa.takatomo@gmail.com>
2023-06-14 16:02:56 +0900

    Update bin/create_indexes.rb, change to output only if the ID is in project_accessions

--- bin/create_indexes.rb
+++ bin/create_indexes.rb
@@ -17,7 +17,15 @@ require_relative 'lib_bioprojectxml2json.rb'
 root_dir = ARGV.length > 0 ? ARGV[0] : '.'
 input_path = File.join(root_dir,'bioproject.xml')
 out_path = File.join(root_dir,'bioproject.json')
+filter_path = File.join(root_dir,'project_accessions')
+require 'csv'
+filter={}
+CSV.foreach(filter_path) do |row|
+    filter[row[0]] = 1
+end
 
+print Time.now, " Started\n"
+unless FileTest.exist?(out_path)
 output_ractor = Ractor.new(out_path) do |out_path|
     json = File.open(out_path, 'a')
     loop do
@@ -29,7 +37,7 @@ output_ractor = Ractor.new(out_path) do |out_path|
     json.close
 end
 
-print Time.now, " Started\n"
+#print Time.now, " Started\n"
 File.delete(out_path) rescue nil
 c = 0
 reader = Nokogiri::XML::EnumParse
@@ -41,30 +49,20 @@ reader.each do |elm|
 end
 output_ractor.send(1)
 print "#{c} packages converted.\n"
-print Time.now, " Finished\n"
----
-#sleep(10)
-##jsonl = '/work1/mdatahub/public/project/bioproject_0313.jsonl'
-#jsonl = 'debug.jsonl'
-#jsonl = 'bioproject_acc_test.jsonl'
-#jsonl = '/work1/mdatahub/public/project/bioproject_0204.jsonl'
-#jsonl = out_path
+print Time.now, " Converted\n"
+end
 
-#index_project = 'mdatahub_index_project-dev.jsonl'
+#---
+ 
 index_project = File.join(root_dir,'mdatahub_index_project.jsonl')
-puts out_path
-puts index_project
-#input_dir = 'testdata/project'
 input_dir =  File.join(root_dir,'project')
 
 File.open(index_project ,mode = "w") do |out_p|
     IO.foreach(out_path) do |line|
         j = JSON.parse(line)
-        #puts line
-        #if bp = j['bioproject'] 
         if j['status'] == 'public'
-            #bp = j
             acc = j['identifier']
+            next unless filter.key? acc
             file = "#{input_dir}/#{acc2path(acc)}/#{acc}-biosampleset.xml" 
             warn file
             if File.exist? file
@@ -79,9 +77,10 @@ File.open(index_project ,mode = "w") do |out_p|
             j['_annotation']['genome_count'] = genome_count
             j['has_analysis'] = has_analysis
             out_p.puts j.to_json
-            #pp j.to_json
         else
+            next unless filter.key? j['index']['_id']
             out_p.puts j.to_json
         end
     end
 end
+print Time.now, " Finished\n"
